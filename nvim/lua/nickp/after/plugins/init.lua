dd5ddd375e22de8fa29767fccdff1471137a7ef6
Nick-Petruccelli <nickpetro18@gmail.com>
2023-10-17 15:36:53 -0400

    init commit

---
+++ nvim/lua/nickp/after/plugins/init.lua
@@ -0,0 +1,3 @@
+require("nickp.after.plugins.colors")
+require("nickp.after.plugins.treesitter")
+require("nickp.after.plugins.telescope")
---
+++ nvim/lua/nickp/settings.lua
@@ -0,0 +1,11 @@
+--line nums
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+--indenting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+
